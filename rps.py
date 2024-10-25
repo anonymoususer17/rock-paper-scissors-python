0c9dac0237d04d6bde5bdcc7ad056fdb4703ba9b
anonymoususer17 <jberobinson.pro@gmail.com>
2024-10-25 15:37:59 +0400

    create a simple interface

--- rps.py
+++ rps.py
@@ -1,35 +1,25 @@
-# Implementing a rock, paper scissors game, with optional lizard and spock
-# I'm going with a simple CLI implementation first to get the (simple) logic down
-
-# importing random for the machine choice
+# Importing random for the machine choice
 import random
 
-# Choose a random number between 1 and 3
-machineChoice = random.choice(range(3))
+class RPSGame:
 
-choices = ["Rock", "Paper", "Scissors"]
-defaultPrompt = "Input the corresponding number for:\n0. Exit\n1. Rock\n2. Paper\n3. Scissors\n"
-
-def startGame():
-    playerChoice = int(input(defaultPrompt))
-    wins = 0
-    losses = 0
-    
-    while playerChoice != 0:
-        machineChoice = random.choice(range(3))
-        if playerChoice not in (1, 2, 3):
-            playerChoice = int(input(f"Try again.\n{defaultPrompt}"))
-        else:
+    def startGame(chosen):
+        playerChoice = chosen
+        wins = 0
+        losses = 0
+        # Initializing default values
+        choices = ["Rock", "Paper", "Scissors"]
+        
+        while playerChoice != 0:
+            # Choose a random number between 1 and 3
+            machineChoice = random.choice(range(3))
             print("Machine choice: " + choices[machineChoice])
             match playerChoice - 1 - machineChoice:
                 case 1 | -2:
-                    print(choices[playerChoice - 1] + " beats " + choices[machineChoice] + ". You win!")
                     wins+=1
+                    return (choices[playerChoice - 1] + " beats " + choices[machineChoice] + ". You win!")
                 case 0:
-                    print("It's a tie")
+                    return ("It's a tie")
                 case _:
-                    print(choices[machineChoice] + " beats " + choices[playerChoice - 1] + ". You lose...")
                     losses+=1
-            playerChoice = int(input(f"Wins: {wins}, Losses: {losses}. Another round?"))
-
-startGame()
+                    return (choices[machineChoice] + " beats " + choices[playerChoice - 1] + ". You lose...")
